987ca21be4a60cbaec35d0922ceebf787c0e7a35
Angel Magalhães <angelsanmag@gmail.com>
2020-05-06 17:06:54 +0100

    First Migration to Database

---
+++ GameListRazor/Migrations/20200506160318_AddGameToDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameListRazor.Migrations
+{
+    // Package Manager Console: add-migration <name>, creates de table
+    // then: update-database, to create the DB in the server specified in the connection string
+    public partial class AddGameToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    GameSystem = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}

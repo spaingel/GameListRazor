671df9d4109bf7fae7ba09a0334eb59f210e3126
Angel Magalhães <angelsanmag@gmail.com>
2020-05-06 16:44:40 +0100

    Add Game class

---
+++ GameListRazor/Model/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GameListRazor.Core
+{
+    class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string GameSystem { get; set; }
+    }
+}

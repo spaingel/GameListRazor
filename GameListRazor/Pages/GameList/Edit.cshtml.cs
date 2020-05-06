93686e379e1ea744e93dce2670b819ecf180a6f4
Angel Magalhães <angelsanmag@gmail.com>
2020-05-06 18:10:17 +0100

    Add Edit Page

---
+++ GameListRazor/Pages/GameList/Edit.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameListRazor.Pages.GameList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Game Game { get; set; }
+
+        public EditModel(ApplicationDbContext db) {
+            _db = db;
+        }
+        public async Task OnGet(int id)
+        {
+            Game = await _db.Game.FindAsync(id);
+        }
+    }
+}

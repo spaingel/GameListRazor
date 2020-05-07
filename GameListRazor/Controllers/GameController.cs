58ad04c977bd9b65a4d570c2c0833c4eb318d58e
Angel Magalhães <angelsanmag@gmail.com>
2020-05-07 10:40:59 +0100

    Delete API Call

--- GameListRazor/Controllers/GameController.cs
+++ GameListRazor/Controllers/GameController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using GameListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameListRazor.Controllers
 {
@@ -18,9 +19,20 @@ namespace GameListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Game.ToList() });
+            return Json(new { data = await _db.Game.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id) {
+            var gameFromDb = await _db.Game.FirstOrDefaultAsync(u => u.Id == id);
+            if (gameFromDb == null) {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Game.Remove(gameFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
         }
     }
 }
